261e4afb52978eea3477fdefee44a34ad0aa98e5
Joy Kavulunze <Joy Kavulunze@ELITEBOOK>
2025-02-19 10:40:30 +0300

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,339 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.eight = New System.Windows.Forms.Button()
+        Me.seven = New System.Windows.Forms.Button()
+        Me.six = New System.Windows.Forms.Button()
+        Me.five = New System.Windows.Forms.Button()
+        Me.four = New System.Windows.Forms.Button()
+        Me.three = New System.Windows.Forms.Button()
+        Me.two = New System.Windows.Forms.Button()
+        Me.one = New System.Windows.Forms.Button()
+        Me.nine = New System.Windows.Forms.Button()
+        Me.zero = New System.Windows.Forms.Button()
+        Me.Decimal_Point = New System.Windows.Forms.Button()
+        Me.division = New System.Windows.Forms.Button()
+        Me.multiply = New System.Windows.Forms.Button()
+        Me.addition = New System.Windows.Forms.Button()
+        Me.subtract = New System.Windows.Forms.Button()
+        Me.Equal_to = New System.Windows.Forms.Button()
+        Me.OFF = New System.Windows.Forms.Button()
+        Me.clear = New System.Windows.Forms.Button()
+        Me.DELETE = New System.Windows.Forms.Button()
+        Me.Input_Output = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'eight
+        '
+        Me.eight.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.eight.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.eight.Font = New System.Drawing.Font("Calisto MT", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.eight.Location = New System.Drawing.Point(85, 69)
+        Me.eight.Name = "eight"
+        Me.eight.Size = New System.Drawing.Size(52, 53)
+        Me.eight.TabIndex = 1
+        Me.eight.Text = "8"
+        Me.eight.UseVisualStyleBackColor = False
+        '
+        'seven
+        '
+        Me.seven.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.seven.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.seven.Font = New System.Drawing.Font("Calisto MT", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.seven.Location = New System.Drawing.Point(162, 69)
+        Me.seven.Name = "seven"
+        Me.seven.Size = New System.Drawing.Size(52, 53)
+        Me.seven.TabIndex = 2
+        Me.seven.Text = "7"
+        Me.seven.UseVisualStyleBackColor = False
+        '
+        'six
+        '
+        Me.six.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.six.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.six.Font = New System.Drawing.Font("Calisto MT", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.six.Location = New System.Drawing.Point(7, 148)
+        Me.six.Name = "six"
+        Me.six.Size = New System.Drawing.Size(52, 53)
+        Me.six.TabIndex = 3
+        Me.six.Text = "6"
+        Me.six.UseVisualStyleBackColor = False
+        '
+        'five
+        '
+        Me.five.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.five.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.five.Font = New System.Drawing.Font("Calisto MT", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.five.Location = New System.Drawing.Point(85, 148)
+        Me.five.Name = "five"
+        Me.five.Size = New System.Drawing.Size(52, 53)
+        Me.five.TabIndex = 4
+        Me.five.Text = "5"
+        Me.five.UseVisualStyleBackColor = False
+        '
+        'four
+        '
+        Me.four.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.four.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.four.Font = New System.Drawing.Font("Calisto MT", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.four.Location = New System.Drawing.Point(162, 148)
+        Me.four.Name = "four"
+        Me.four.Size = New System.Drawing.Size(52, 53)
+        Me.four.TabIndex = 5
+        Me.four.Text = "4"
+        Me.four.UseVisualStyleBackColor = False
+        '
+        'three
+        '
+        Me.three.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.three.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.three.Font = New System.Drawing.Font("Calisto MT", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.three.Location = New System.Drawing.Point(7, 229)
+        Me.three.Name = "three"
+        Me.three.Size = New System.Drawing.Size(52, 53)
+        Me.three.TabIndex = 6
+        Me.three.Text = "3"
+        Me.three.UseVisualStyleBackColor = False
+        '
+        'two
+        '
+        Me.two.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.two.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.two.Font = New System.Drawing.Font("Calisto MT", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.two.Location = New System.Drawing.Point(85, 229)
+        Me.two.Name = "two"
+        Me.two.Size = New System.Drawing.Size(52, 53)
+        Me.two.TabIndex = 7
+        Me.two.Text = "2"
+        Me.two.UseVisualStyleBackColor = False
+        '
+        'one
+        '
+        Me.one.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.one.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.one.Font = New System.Drawing.Font("Calisto MT", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.one.Location = New System.Drawing.Point(162, 229)
+        Me.one.Name = "one"
+        Me.one.Size = New System.Drawing.Size(52, 53)
+        Me.one.TabIndex = 8
+        Me.one.Text = "1"
+        Me.one.UseVisualStyleBackColor = False
+        '
+        'nine
+        '
+        Me.nine.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.nine.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.nine.Font = New System.Drawing.Font("Calisto MT", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nine.Location = New System.Drawing.Point(7, 69)
+        Me.nine.Name = "nine"
+        Me.nine.Size = New System.Drawing.Size(52, 53)
+        Me.nine.TabIndex = 0
+        Me.nine.Text = "9"
+        Me.nine.UseVisualStyleBackColor = False
+        '
+        'zero
+        '
+        Me.zero.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.zero.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.zero.Font = New System.Drawing.Font("Calisto MT", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.zero.Location = New System.Drawing.Point(7, 305)
+        Me.zero.Name = "zero"
+        Me.zero.Size = New System.Drawing.Size(52, 53)
+        Me.zero.TabIndex = 9
+        Me.zero.Text = "0"
+        Me.zero.UseVisualStyleBackColor = False
+        '
+        'Decimal_Point
+        '
+        Me.Decimal_Point.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Decimal_Point.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Decimal_Point.Font = New System.Drawing.Font("Calisto MT", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Decimal_Point.Location = New System.Drawing.Point(85, 305)
+        Me.Decimal_Point.Name = "Decimal_Point"
+        Me.Decimal_Point.Size = New System.Drawing.Size(52, 53)
+        Me.Decimal_Point.TabIndex = 11
+        Me.Decimal_Point.Text = "."
+        Me.Decimal_Point.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Decimal_Point.UseVisualStyleBackColor = False
+        '
+        'division
+        '
+        Me.division.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.division.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.division.Font = New System.Drawing.Font("Calisto MT", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.division.Location = New System.Drawing.Point(238, 69)
+        Me.division.Name = "division"
+        Me.division.Size = New System.Drawing.Size(52, 53)
+        Me.division.TabIndex = 12
+        Me.division.Text = "/"
+        Me.division.UseVisualStyleBackColor = False
+        '
+        'multiply
+        '
+        Me.multiply.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.multiply.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.multiply.Font = New System.Drawing.Font("Calisto MT", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.multiply.Location = New System.Drawing.Point(238, 148)
+        Me.multiply.Name = "multiply"
+        Me.multiply.Size = New System.Drawing.Size(52, 53)
+        Me.multiply.TabIndex = 13
+        Me.multiply.Text = "*"
+        Me.multiply.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.multiply.UseVisualStyleBackColor = False
+        '
+        'addition
+        '
+        Me.addition.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.addition.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.addition.Font = New System.Drawing.Font("Calisto MT", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.addition.Location = New System.Drawing.Point(238, 229)
+        Me.addition.Name = "addition"
+        Me.addition.Size = New System.Drawing.Size(52, 53)
+        Me.addition.TabIndex = 14
+        Me.addition.Text = "+"
+        Me.addition.UseVisualStyleBackColor = False
+        '
+        'subtract
+        '
+        Me.subtract.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.subtract.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.subtract.Font = New System.Drawing.Font("Calisto MT", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.subtract.Location = New System.Drawing.Point(162, 305)
+        Me.subtract.Name = "subtract"
+        Me.subtract.Size = New System.Drawing.Size(52, 53)
+        Me.subtract.TabIndex = 15
+        Me.subtract.Text = "-"
+        Me.subtract.UseVisualStyleBackColor = False
+        '
+        'Equal_to
+        '
+        Me.Equal_to.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Equal_to.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Equal_to.Font = New System.Drawing.Font("Calisto MT", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Equal_to.Location = New System.Drawing.Point(238, 305)
+        Me.Equal_to.Name = "Equal_to"
+        Me.Equal_to.Size = New System.Drawing.Size(134, 53)
+        Me.Equal_to.TabIndex = 16
+        Me.Equal_to.Text = "="
+        Me.Equal_to.UseVisualStyleBackColor = False
+        '
+        'OFF
+        '
+        Me.OFF.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.OFF.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.OFF.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OFF.Location = New System.Drawing.Point(311, 69)
+        Me.OFF.Name = "OFF"
+        Me.OFF.Size = New System.Drawing.Size(52, 53)
+        Me.OFF.TabIndex = 17
+        Me.OFF.Text = "OFF"
+        Me.OFF.UseVisualStyleBackColor = False
+        '
+        'clear
+        '
+        Me.clear.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.clear.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.clear.Font = New System.Drawing.Font("Calisto MT", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clear.Location = New System.Drawing.Point(311, 148)
+        Me.clear.Name = "clear"
+        Me.clear.Size = New System.Drawing.Size(52, 53)
+        Me.clear.TabIndex = 18
+        Me.clear.Text = "C"
+        Me.clear.UseVisualStyleBackColor = False
+        '
+        'DELETE
+        '
+        Me.DELETE.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.DELETE.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.DELETE.Font = New System.Drawing.Font("Lucida Console", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DELETE.Location = New System.Drawing.Point(311, 229)
+        Me.DELETE.Name = "DELETE"
+        Me.DELETE.Size = New System.Drawing.Size(61, 53)
+        Me.DELETE.TabIndex = 19
+        Me.DELETE.Text = "DELETE"
+        Me.DELETE.UseVisualStyleBackColor = False
+        '
+        'Input_Output
+        '
+        Me.Input_Output.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Input_Output.Font = New System.Drawing.Font("Castellar", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Input_Output.Location = New System.Drawing.Point(7, 4)
+        Me.Input_Output.Multiline = True
+        Me.Input_Output.Name = "Input_Output"
+        Me.Input_Output.Size = New System.Drawing.Size(365, 59)
+        Me.Input_Output.TabIndex = 20
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.ClientSize = New System.Drawing.Size(378, 361)
+        Me.Controls.Add(Me.Input_Output)
+        Me.Controls.Add(Me.DELETE)
+        Me.Controls.Add(Me.clear)
+        Me.Controls.Add(Me.OFF)
+        Me.Controls.Add(Me.Equal_to)
+        Me.Controls.Add(Me.subtract)
+        Me.Controls.Add(Me.addition)
+        Me.Controls.Add(Me.multiply)
+        Me.Controls.Add(Me.division)
+        Me.Controls.Add(Me.Decimal_Point)
+        Me.Controls.Add(Me.zero)
+        Me.Controls.Add(Me.one)
+        Me.Controls.Add(Me.two)
+        Me.Controls.Add(Me.three)
+        Me.Controls.Add(Me.four)
+        Me.Controls.Add(Me.five)
+        Me.Controls.Add(Me.six)
+        Me.Controls.Add(Me.seven)
+        Me.Controls.Add(Me.eight)
+        Me.Controls.Add(Me.nine)
+        Me.Name = "Form1"
+        Me.Text = " CALCULATOR APP"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents eight As Button
+    Friend WithEvents seven As Button
+    Friend WithEvents six As Button
+    Friend WithEvents five As Button
+    Friend WithEvents four As Button
+    Friend WithEvents three As Button
+    Friend WithEvents two As Button
+    Friend WithEvents one As Button
+    Friend WithEvents nine As Button
+    Friend WithEvents zero As Button
+    Friend WithEvents Decimal_Point As Button
+    Friend WithEvents division As Button
+    Friend WithEvents multiply As Button
+    Friend WithEvents addition As Button
+    Friend WithEvents subtract As Button
+    Friend WithEvents Equal_to As Button
+    Friend WithEvents OFF As Button
+    Friend WithEvents clear As Button
+    Friend WithEvents DELETE As Button
+    Friend WithEvents Input_Output As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,88 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles nine.Click
+
+    End Sub
+
+    Private Sub eight_Click(sender As Object, e As EventArgs) Handles eight.Click
+
+    End Sub
+
+    Private Sub seven_Click(sender As Object, e As EventArgs) Handles seven.Click
+
+    End Sub
+
+    Private Sub six_Click(sender As Object, e As EventArgs) Handles six.Click
+
+    End Sub
+
+    Private Sub five_Click(sender As Object, e As EventArgs) Handles five.Click
+
+    End Sub
+
+    Private Sub four_Click(sender As Object, e As EventArgs) Handles four.Click
+
+    End Sub
+
+    Private Sub three_Click(sender As Object, e As EventArgs) Handles three.Click
+
+    End Sub
+
+    Private Sub two_Click(sender As Object, e As EventArgs) Handles two.Click
+
+    End Sub
+
+    Private Sub one_Click(sender As Object, e As EventArgs) Handles one.Click
+
+    End Sub
+
+    Private Sub zero_Click(sender As Object, e As EventArgs) Handles zero.Click
+
+    End Sub
+
+
+    ' Start of  the basic arithmetic manipulation Functions'
+
+    Private Sub division_Click(sender As Object, e As EventArgs) Handles division.Click
+
+    End Sub
+
+    Private Sub multiply_Click(sender As Object, e As EventArgs) Handles multiply.Click
+
+    End Sub
+
+    Private Sub addition_Click(sender As Object, e As EventArgs) Handles addition.Click
+
+    End Sub
+
+    Private Sub subtract_Click(sender As Object, e As EventArgs) Handles subtract.Click
+
+    End Sub
+
+    'Output Manipulation Functions'
+
+    Private Sub Equal_to_Click(sender As Object, e As EventArgs) Handles Equal_to.Click
+
+    End Sub
+
+    Private Sub OFF_Click(sender As Object, e As EventArgs) Handles OFF.Click
+
+    End Sub
+
+    Private Sub clear_Click(sender As Object, e As EventArgs) Handles clear.Click
+
+    End Sub
+
+    Private Sub DELETE_Click(sender As Object, e As EventArgs) Handles DELETE.Click
+
+    End Sub
+
+    'Input Manipulation Functions'
+
+    Private Sub Input_Output_TextChanged(sender As Object, e As EventArgs) Handles Input_Output.TextChanged
+
+    End Sub
+End Class
